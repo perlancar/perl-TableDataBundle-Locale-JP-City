ea6cb24330e801d70333a75d55ed3ad6246d0f1b
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-08-25 20:23:47 +0700

    Initial commit

---
+++ lib/TableDataBundle/Locale/JP/City.pm
@@ -0,0 +1,32 @@
+package TableDataBundle::Locale::JP::City;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Collection of TableData:: modules that contain list of cities in Japan
+
+=head1 DESCRIPTION
+
+This distribution contains the following L<TableData>:: modules:
+
+# INSERT_MODULES_LIST /^TableData::/
+
+
+=head1 SEE ALSO
+
+L<TableData>
+
+Other C<TableDataBundle::Locale::*::City> or
+C<TableDataBundle::Locale::*::Locality> distributions.
+
+L<https://en.wikipedia.org/wiki/List_of_cities_in_Japan> which contains about
+800 cities, plus some cities that have been dissolved. Each record has romanized
+name, Japanese name, prefecture name, population, area, founding date, as well
+as link to official website.
+
+=cut
